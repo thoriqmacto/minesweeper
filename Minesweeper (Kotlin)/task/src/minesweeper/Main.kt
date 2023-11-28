37528ba1a66c6e98d84879d1a32f0f2cd3bb8462
thoriqmacto <thariq.hadad@gmail.com>
2023-11-28 23:16:14 +0700

    finish stage-1

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -1,5 +1,58 @@
 package minesweeper
 
+import kotlin.random.Random
+
 fun main() {
-    print("Hello, World!")
+    val battleField = createBattleField(9,9,10)
+}
+
+fun createBattleField(row:Int,col:Int,mines:Int): MutableList<MutableList<Char>> {
+    val outputList = MutableList(row){MutableList(col){'.'} }
+    val minesCoordinate = generateMinesCoordinate(mines,row,col)
+
+//    println("Original matrix:")
+//    outputList.forEach { it ->
+//        println(it.joinToString(""))
+//    }
+//    for (i in 0 until row){
+//        val rowTemp = mutableListOf<Char>()
+//        for (j in (0 until col)){
+//            rowTemp += '.'
+//            print(".")
+//        }
+//        outputList += rowTemp
+//        println()
+//    }
+
+    minesCoordinate.forEach { it ->
+        val coordinate = it.split("|").map { it.toInt() }
+        val rowMine = coordinate[0]
+        val colMine = coordinate[1]
+//        println(coordinate)
+        outputList[rowMine][colMine] = 'X'
+//        println(outputList[rowMine][colMine])
+    }
+
+//    println("After update :")
+    outputList.forEach { it ->
+        println(it.joinToString(""))
+    }
+
+    return outputList
+}
+
+fun generateMinesCoordinate(numMines:Int,row:Int, col: Int):MutableList<String>{
+    val output = mutableListOf<String>()
+
+    for (i in 0 until numMines){
+        val randomRow = Random.nextInt(0,row)
+        val randomCol = Random.nextInt(0,col)
+
+        while (!output.contains("$randomRow|$randomCol")){
+            output.add("$randomRow|$randomCol")
+//            println("$randomRow|$randomCol")
+        }
+    }
+
+    return output
 }
---
+++ Topics/Random/RPG dices/src/Main.kt
@@ -0,0 +1,7 @@
+import kotlin.random.Random
+
+fun rpgDices(dice1: Int, dice2: Int): Int {
+    // write your code here
+
+    return sumOfDices
+}
