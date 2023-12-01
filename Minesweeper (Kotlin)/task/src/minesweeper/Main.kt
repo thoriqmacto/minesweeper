5431e93394501c0ece73c06108dd3d293d99cd21
thoriqmacto <thariq.hadad@gmail.com>
2023-12-01 15:10:00 +0700

    finish stage 3

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -12,30 +12,18 @@ fun createBattleField(row:Int,col:Int,mines:Int): MutableList<MutableList<Char>>
     val outputList = MutableList(row){MutableList(col){'.'} }
     val minesCoordinate = generateMinesCoordinate(mines,row,col)
 
-//    println("Original matrix:")
-//    outputList.forEach { it ->
-//        println(it.joinToString(""))
-//    }
-//    for (i in 0 until row){
-//        val rowTemp = mutableListOf<Char>()
-//        for (j in (0 until col)){
-//            rowTemp += '.'
-//            print(".")
-//        }
-//        outputList += rowTemp
-//        println()
-//    }
-
+    // placing mines
     minesCoordinate.forEach { it ->
         val coordinate = it.split("|").map { it.toInt() }
         val rowMine = coordinate[0]
         val colMine = coordinate[1]
-//        println(coordinate)
         outputList[rowMine][colMine] = 'X'
-//        println(outputList[rowMine][colMine])
+
+        // put number surround mines
+        updateAdjacentCells(row,col,rowMine,colMine,outputList)
     }
 
-//    println("After update :")
+    // print all
     outputList.forEach { it ->
         println(it.joinToString(""))
     }
@@ -52,9 +40,38 @@ fun generateMinesCoordinate(numMines:Int,row:Int, col: Int):MutableList<String>{
 
         if (!output.contains("$randomRow|$randomCol")) {
             output.add("$randomRow|$randomCol")
-//            println("$randomRow|$randomCol")
         }
     }while (output.size < numMines)
 
     return output
+}
+
+fun updateAdjacentCells(totalRow: Int, totalCol: Int,
+                        rowMine: Int, colMine: Int,
+                        outputList: MutableList<MutableList<Char>>) {
+    val adjacentCells = mutableListOf<Pair<Int, Int>>()
+
+    // Determine adjacent cells based on the mine position
+    for (i in -1..1) {
+        for (j in -1..1) {
+            val newRow = rowMine + i
+            val newCol = colMine + j
+
+            if ((i != 0 || j != 0) && newRow in 0 until totalRow && newCol in 0 until totalCol) {
+                adjacentCells.add(Pair(newRow, newCol))
+            }
+        }
+    }
+
+    // Update adjacent cells
+    adjacentCells.forEach { (r, c) ->
+        val currentChar = outputList[r][c]
+        if (currentChar.isDigit()) {
+            var tempInt = currentChar.digitToInt()
+            tempInt++
+            outputList[r][c] = tempInt.digitToChar()
+        }else if(currentChar != 'X' && currentChar == '.'){
+            outputList[r][c] = '1'
+        }
+    }
 }
